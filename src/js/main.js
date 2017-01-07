4b72d941abfab80ede90f0ebaa4944bdafb35ff7
itomblack <itomblack@hotmail.co.uk>
2017-01-07 18:46:26 +0000

    today tomorrow basic

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
 	//set location
 	var latitude = 51.5074;
 	var longitude = 0.1278;
+	var tomorrow = '2016-01-08T00:00:00' 
 
 	//set times to collect and peak commute times
 	var timesOfDay = [{  
@@ -47,29 +48,30 @@ $( document ).ready(function() {
 
 	// Get weather info from API
 	if ( $('#weather-body').length ) {
-		apiCall();
-	  //which will call afterGotData if successful
+		apiCall('now'); //which will call afterGotData if successful
 	}
 
-	function afterGotData() {
+	function afterGotData(startHour) {
 		//***** 0 = current hour ***** 1 per hours ***** 24 = next day current hour ***** 48 = 2 days time current hour ***//
 		var locationName = apiCallData.timezone;
 		var currentTempC = apiCallData.currently.temperature;
-		var timeStamp = timeConverter(apiCallData.hourly.data[0].time);
-
-		console.log(locationName);
-		console.log(currentTempC + ' C');
-		console.log('time is ' + timeStamp[0].timeHour + ':00');
-		console.log('date is ' + timeStamp[0].dateDay + ' ' + timeStamp[0].dateMonth);
+		var timeStamp = timeConverter(apiCallData.hourly.data[startHour].time);
 
 		$('#time').html(timeStamp[0].timeHour + ':00');
 		$('#date').html(timeStamp[0].dateDay + ' ' + timeStamp[0].dateMonth);
 		$('#location').html(locationName);
 		$('#currentTemp').html(currentTempC + ' &degC');
 
-
-		// console.log(apiCallData);
 		// work out what temperature bracket we are in (from current temp)
+		if (currentTempC <= 0) { inRange(0);	}
+		else if (currentTempC <= 5) { inRange(1); }
+		else if (currentTempC <= 10) { inRange(2); }
+		else if (currentTempC <= 15) { inRange(3); }
+		else if (currentTempC <= 20) { inRange(4); }
+		else if (currentTempC <= 25) { inRange(5); }
+		else if (currentTempC <= 30) { inRange(6); }
+		else if (currentTempC > 30) { inRange(7); }
+
 
 		// work out whether it's a wet or a dry day (from travel times)
 
@@ -78,26 +80,47 @@ $( document ).ready(function() {
 
 
 
+	// ************* WORK OUT WHICH RANGE THE TEMP IS IN **************** //
+	// ************* *************************************** **************** //
+	function inRange(rangeNum) {
+		if (rangeNum == 0) { $('#rating').text('Cold as ice!'); } 
+		else if (rangeNum == 1) { $('#rating').text('Damn cold'); } 
+		else if (rangeNum == 2) { $('#rating').text('Pretty nippy'); } 
+		else if (rangeNum == 3) { $('#rating').text('Faily mild out'); } 
+		else if (rangeNum == 4) { $('#rating').text('Just about right'); } 
+		else if (rangeNum == 5) { $('#rating').text('Warm enough'); } 
+		else if (rangeNum == 6) { $('#rating').text('Pretty sweaty'); } 
+		else if (rangeNum == 7) { $('#rating').text('Melt your balls off!'); } 
+	}
 
 
 
+	// ************* CALLING THE API DARKSKY **************** //
+	// ************* *********************** **************** //
+	function apiCall(when) {
 
+		var urlNow = 'https://api.forecast.io/forecast/e4e49949156eaa4a4addd8e71c9bed0b/' + latitude + ',' + longitude + '?units=si&exclude=minutely+daily+flags+alerts';
+		var urlTomorrow = 'https://api.forecast.io/forecast/e4e49949156eaa4a4addd8e71c9bed0b/' + latitude + ',' + longitude + ',' + tomorrow + '?units=si&exclude=minutely+daily+flags+alerts';
+		
+		//choose which API call
+		if (when == 'now') {
+			urlToUse = urlNow
+			startHour = 0;
+		}
+		else if (when == 'tomorrow') { 
+			urlToUse = urlTomorrow 
+			startHour = 8;
+		}
 
-
-
-
-	
-
-
-	function apiCall() {
+		//call API
 		$.ajax({
-		  url: 'https://api.forecast.io/forecast/e4e49949156eaa4a4addd8e71c9bed0b/' + latitude + ',' + longitude + '?units=si&exclude=minutely+daily+flags+alerts',
+		  url: urlToUse,
 		  dataType: 'jsonp',
 		  success: function (data) {
 		  		//store data in variable
 		      apiCallData = data;
 		      //continue next steps
-		      afterGotData();
+		      afterGotData(startHour);
 		  },
 		  error: function() {
 		  	console.log('Oops, an error occured');
@@ -141,6 +164,12 @@ $( document ).ready(function() {
 	  }]
 	  return time;
 	}
+
+
+	// ************* CLICK TOMORROW CALLS TOMORROW DATA **************** //
+	// ************* ********************************* **************** //
+	$('#btnTomorrow').click(function() { apiCall('tomorrow'); })
+	$('#btnNow').click(function() { apiCall('now'); })
 
 
 }); //END ALL DOCUMENT
